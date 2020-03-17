0f198847b33e40e4fc48244ebc4a8c879f2d28e8
Linus Pahl <lpahl@protonmail.com>
2020-03-17 13:31:10 +0100

    Extend CSV export modal

--- graylog2-web-interface/src/views/components/searchbar/CSVExportModal.jsx
+++ graylog2-web-interface/src/views/components/searchbar/CSVExportModal.jsx
@@ -1,97 +1,113 @@
 // @flow strict
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { sortBy } from 'lodash';
-import URI from 'urijs';
-import connect from 'stores/connect';
-/* $FlowFixMe: Need to add to flow typed */
+import CustomPropTypes from 'views/components/CustomPropTypes';
+import * as Immutable from 'immutable';
 import styled from 'styled-components';
 
-import { StreamsStore } from 'views/stores/StreamsStore';
+import type { ViewStoreState } from 'views/stores/ViewStore';
+
+import connect from 'stores/connect';
 import { FieldTypesStore } from 'views/stores/FieldTypesStore';
-import { ViewStore } from 'views/stores/ViewStore';
+import SortDirectionSelect from 'views/components/widgets/SortDirectionSelect';
+import { defaultCompare } from 'views/logic/DefaultCompare';
 import URLUtils from 'util/URLUtils';
 import { Modal, Button, Row } from 'components/graylog';
 import BootstrapModalWrapper from 'components/bootstrap/BootstrapModalWrapper';
-import { Icon } from 'components/common';
-import ApiRoutes from 'routing/ApiRoutes';
-import StoreProvider from 'injection/StoreProvider';
-import View from 'views/logic/views/View';
+import FieldSortSelect from 'views/components/widgets/FieldSortSelect';
+import SortableSelect from 'views/components/aggregationbuilder/SortableSelect';
+
 import Select from 'views/components/Select';
+import FieldTypeMapping from 'views/logic/fieldtypes/FieldTypeMapping';
 
-const SessionStore = StoreProvider.getStore('Session');
-
-type Option = {
-  label: String,
-  value: String,
-};
+const ValueComponent = styled.span`
+  padding: 2px 5px;
+`;
 
 type Props = {
-  availableStreams: Array<Option>,
-  availableFields: Array<Option>,
+  fields: Immutable.List<FieldTypeMapping>,
   closeModal: () => void,
+  currentWidgetId?: string,
+  view: ViewStoreState
 };
 
-const getURLForExportAsCSV = (selectedStream: ?string, selectedFields: Array<string> = []) => {
-  const { view } = ViewStore.getInitialState() || {};
-  if (view && view.type === View.Type.Search) {
-    const { queries } = view.search;
-    if (queries.size !== 1) {
-      throw new Error('Searches must only have a single query!');
-    }
-    const firstQuery = queries.first();
-    if (firstQuery) {
-      const { query: { query_string: queryString }, timerange } = firstQuery;
-      const query = !queryString ? '*' : queryString;
-      const url = new URI(URLUtils.qualifyUrl(
-        ApiRoutes.UniversalSearchApiController.export(
-          timerange.type,
-          query,
-          timerange,
-          selectedStream,
-          0,
-          0,
-          selectedFields,
-        ).url,
-      ));
-      if (URLUtils.areCredentialsInURLSupported()) {
-        url
-          .username(SessionStore.getSessionId())
-          .password('session');
-      }
+const Content = styled.div`
+  margin-left: 10px;
+  margin-right: 10px;
+`;
 
-      return url.toString();
-    }
-  }
-  return new URI(URLUtils.qualifyUrl('/notfound'));
-};
-
-const wrapOption = o => ({ label: o, value: o });
+// const wrapOption = o => ({ label: o, value: o });
+// const wrapOption = o => ({ field: o });
 const defaultFields = ['timestamp', 'source', 'message'];
-const defaultFieldOptions = defaultFields.map(wrapOption);
+// const defaultFieldOptions = defaultFields.map(wrapOption);
 
-const CSVExportModal = ({ closeModal, availableStreams, availableFields }: Props) => {
-  const [selectedStream, setSelectedStream] = useState();
-  const [selectedFields, setSelectedFields] = useState(defaultFieldOptions);
-
-  const link = selectedFields.length > 0
-    ? (
-      <a href={getURLForExportAsCSV((selectedStream || {}).value, selectedFields.map(f => f.value))} target="_parent">
-        <Icon name="cloud-download" />&nbsp;
-        Download
-      </a>
-    )
-    : <p>Select at least on field to export messages as CSV.</p>;
-
-  const infoText = (URLUtils.areCredentialsInURLSupported()
-    ? 'Please right click the download link below and choose "Save Link As..." to download the CSV file.'
-    : 'Please click the download link below. Your browser may ask for your username and password to '
+const infoText = (URLUtils.areCredentialsInURLSupported()
+  ? 'Please right click the download link below and choose "Save Link As..." to download the CSV file.'
+  : 'Please click the download link below. Your browser may ask for your username and password to '
     + 'download the CSV file.');
 
-  const Content = styled.div`
-    margin-left: 10px;
-    margin-right: 10px;
-  `;
+const _onSortDirectionChange = (newDirection, selectedSort, setSelectedSort) => {
+  const newSort = selectedSort.map(sort => sort.toBuilder().direction(newDirection).build());
+  setSelectedSort(newSort);
+};
+
+const _widgetLabel = (widgetTitles, widgetId) => {
+  return widgetId;
+  // return widgetTitles.get(widgetId);
+};
+
+const CSVExportModal = ({ closeModal, fields, view, currentWidgetId }: Props) => {
+  const messageWidgets = view.view.state.reduce((result, viewState) => {
+    let stateWidgets = Immutable.Map();
+    viewState.widgets.forEach((widget) => {
+      if (widget.type === 'messages') {
+        stateWidgets = stateWidgets.set(widget.id, widget);
+      }
+    });
+    return result.merge(stateWidgets);
+  }, Immutable.Map());
+  const widgetTitles = view.view.state.reduce((result, viewState) => {
+    const stateTitles = Immutable.Map();
+    // viewState.titles.get('widget').forEach((title, key) => {
+    //   stateTitles = stateTitles.set(key, title);
+    // });
+    return result.merge(stateTitles);
+  }, Immutable.Map());
+
+  const currentWidget = currentWidgetId ? messageWidgets.find(widget => widget.id === currentWidgetId) : messageWidgets.first();
+
+
+  const widgetOptions = messageWidgets.map(widget => ({ label: _widgetLabel(widgetTitles, widget.id), value: widget }));
+
+
+  const [selectedFields, setSelectedFields] = useState(currentWidget ? currentWidget.config.fields.map(fieldName => ({ field: fieldName })) : defaultFields.map(fieldName => ({ field: fieldName })));
+  const [selectedWidget, setSelectedWidget] = useState(null);
+  const [selectedSort, setSelectedSort] = useState(currentWidget ? currentWidget.config.sort : []);
+  const [sortDirection] = (selectedSort || []).map(s => s.direction);
+  const multiSelect = !currentWidgetId && messageWidgets.size > 1;
+
+  const _onApplyWidgetSettings = ({ value: newWidget }) => {
+    setSelectedWidget(newWidget);
+    setSelectedFields(newWidget.config.fields.map(fieldName => ({ field: fieldName })));
+    setSelectedSort(newWidget.config.sort);
+  };
+
+  const _onFieldSelect = (newFields) => {
+    setSelectedFields(newFields.map(field => ({ field: field.value })));
+  };
+
+  const _onSortSelect = (newSort) => {
+    setSelectedSort(newSort);
+  };
+
+  const fieldsForSelect = fields
+    .map(fieldType => fieldType.name)
+    .map(fieldName => ({ label: fieldName, value: fieldName }))
+    .valueSeq()
+    .toJS()
+    .sort((v1, v2) => defaultCompare(v1.label, v2.label));
+
+  const startDownload = () => {};
   return (
     <BootstrapModalWrapper showModal>
       <Modal.Header>
@@ -102,28 +118,36 @@ const CSVExportModal = ({ closeModal, availableStreams, availableFields }: Props
           <Row>
             <p>{infoText}</p>
           </Row>
-          <Row>
-            <span>Add stream to filter messages:</span>
-            <Select placeholder="None: click to add stream"
-                    onChange={selection => setSelectedStream(selection)}
-                    options={availableStreams}
-                    value={selectedStream} />
-          </Row>
+          {multiSelect && (
+            <Row>
+              <span>Adopt message table settings:</span>
+              <Select placeholder="Select widget to adopt settings"
+                      onChange={selection => _onApplyWidgetSettings(selection)}
+                      options={widgetOptions}
+                      value={selectedWidget ? { value: selectedWidget, label: selectedWidget.id } : null} />
+            </Row>
+          )}
           <Row>
             <span>Select fields to export:</span>
-            <Select placeholder="None: click to add fields"
-                    onChange={selection => setSelectedFields(selection)}
-                    options={availableFields}
-                    value={selectedFields}
-                    isMulti />
+            <SortableSelect options={fieldsForSelect}
+                            onChange={newFields => _onFieldSelect(newFields)}
+                            valueComponent={({ children: _children }) => <ValueComponent>{_children}</ValueComponent>}
+                            value={selectedFields} />
           </Row>
           <Row>
-            {link}
+            <span>Select sort:</span>
+            <FieldSortSelect fields={fields} sort={selectedSort} onChange={sort => _onSortSelect(sort)} />
+          </Row>
+          <Row>
+            <span>Select sort direction:</span>
+            <SortDirectionSelect direction={sortDirection ? sortDirection.direction : null}
+                                 onChange={newDirection => _onSortDirectionChange(newDirection, selectedSort, setSelectedSort)} />
           </Row>
         </Content>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={closeModal}>Close</Button>
+        <Button type="button" onClick={closeModal}>Close</Button>
+        <Button type="button" onClick={startDownload} bsStyle="primary">Start Download</Button>
       </Modal.Footer>
     </BootstrapModalWrapper>
   );
@@ -131,25 +155,22 @@ const CSVExportModal = ({ closeModal, availableStreams, availableFields }: Props
 
 CSVExportModal.propTypes = {
   closeModal: PropTypes.func,
-  availableStreams: PropTypes.array,
-  availableFields: PropTypes.array,
+  fields: CustomPropTypes.FieldListType.isRequired,
+  currentWidgetId: PropTypes.string,
 };
 
 CSVExportModal.defaultProps = {
   closeModal: () => {},
-  availableStreams: [],
-  availableFields: [],
+  currentWidgetId: null,
 };
 
 export default connect(
   CSVExportModal,
   {
-    availableStreams: StreamsStore,
-    availableFields: FieldTypesStore,
+    fields: FieldTypesStore,
   },
-  ({ availableStreams: { streams }, availableFields: { all }, ...rest }) => ({
+  ({ fields: { all }, ...rest }) => ({
     ...rest,
-    availableStreams: sortBy(streams.map(stream => ({ label: stream.title, value: stream.id })), ['label']),
-    availableFields: all.map(field => ({ label: field.name, value: field.name })).sortBy(f => f.label).toArray(),
+    fields: all,
   }),
 );
